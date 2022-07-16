2f5e24e4c330d4e5e28f9a25653148a552a77045
RobertoAlve <roberto.alves@bandtec.com.br>
2022-07-16 14:28:07 -0300

    Adicionando projeto

---
+++ 21.de.valores/src/test/java/br/com/valores1/de/valores/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valores1.de.valores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
